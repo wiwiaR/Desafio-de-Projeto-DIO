026eaf1ab4964ae96a90749a41c0dbe2de7fb7b3
Vitória Ribeiro <wiwia_ribeiro@icloud.com>
2022-01-21 09:57:39 -0300

    Insercao de arquivos pra criacao de site

---
+++ script_jquery.js
@@ -0,0 +1,27 @@
+
+$(function(){
+	$('#l1').click(function(){
+		$('#i1').show()
+		$('#i2').hide()
+		$('#i3').hide()
+		$('#i4').hide()
+	})
+	$('#l2').click(function(){
+		$('#i1').hide()
+		$('#i2').show()
+		$('#i3').hide()
+		$('#i4').hide()
+	})
+	$('#l3').click(function(){
+		$('#i1').hide()
+		$('#i2').hide()
+		$('#i3').show()
+		$('#i4').hide()
+	})
+	$('#l4').click(function(){
+		$('#i1').hide()
+		$('#i2').hide()
+		$('#i3').hide()
+		$('#i4').show()
+	})
+})
